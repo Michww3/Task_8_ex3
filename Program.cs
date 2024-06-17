70ed41736097e07eba14b993b163e53e9108173b
User <User@W11PC>
2024-06-17 09:59:45 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml.Serialization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(Dog));
+        using (FileStream fileStream = new FileStream("User_Atr.xml", FileMode.OpenOrCreate))
+        {
+            Dog dog = serializer.Deserialize(fileStream) as Dog;
+            Console.WriteLine(dog.name + dog.age);
+        }
+
+    }
+}
+
+public class Dog
+{
+    [XmlAttribute]
+    public string name;
+    [XmlAttribute]
+    public int age;
+
+    public Dog(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    public Dog()
+    {
+
+    }
+}
